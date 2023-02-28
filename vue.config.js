4b1d6ef3300d7467e15d3bf840fa781e3a2a8202
彭成刚 <76526211@qq.com>
2023-02-28 08:28:02 +0800

    都删了 从来 不要pretty

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-const BASE_URL =
-  process.env.NODE_ENV === "production" ? "/fastwork/" : "/fastwork/";
-module.exports = defineConfig({
-  publicPath: BASE_URL,
-  transpileDependencies: true,
-});
