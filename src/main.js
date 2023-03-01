807766b89c7d5407596baa6cd134ff24b2bed188
彭成刚 <76526211@qq.com>
2023-03-01 10:06:35 +0800

    buildFastByGiteeUpdate

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ViewUI from 'view-design'
+// import ViewUI from 'view-design'
+import { Button, Table, Menu, MenuItem, Layout } from 'view-design'
 import 'view-design/dist/styles/iview.css'
-
-Vue.use(ViewUI)
+Vue.component('Button', Button)
+Vue.component('Table', Table)
+Vue.component('Menu', Menu)
+Vue.component('MenuItem', MenuItem)
+Vue.component('Layout', Layout)
+// Vue.use(ViewUI)
 
 Vue.config.productionTip = false
 
