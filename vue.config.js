33071d2107257ce3fc68fc60525265524d54690d
彭成刚 <76526211@qq.com>
2023-03-06 20:33:14 +0800

    buildFastByGiteeUpdate

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+let ret = ''
+const date = new Date()
+ret += date.getFullYear()
+ret += '-' + (date.getMonth() + 1)
+ret += '-' + date.getDate()
+ret += '_' + date.getHours()
+ret += '-' + date.getMinutes()
+ret += '-' + date.getSeconds()
+const timeStamp = ret
 module.exports = defineConfig({
+  configureWebpack: {
+    // devtool: 'source-map',
+    output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+      filename: `js/[name].${timeStamp}.js`,
+      chunkFilename: `js/[name].${timeStamp}.js`
+    }
+  },
   productionSourceMap: false, // 打包不生成map
   publicPath: '/fastwork/',
   transpileDependencies: true
