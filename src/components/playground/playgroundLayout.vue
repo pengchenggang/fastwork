e83ede92cfd8072dfede5b1e55d392d188290c9a
彭成刚 <76526211@qq.com>
2023-03-07 11:13:16 +0800

    代码的playground可算是能用了

--- src/components/playground/playgroundLayout.vue
+++ src/components/playground/playgroundLayout.vue
@@ -3,6 +3,7 @@
     <Card>
       <Row>
         <Col span="12"
+             ref="leftRef"
              style="border-right: 1px dashed #ccc;">
         <div style="padding: 16px;">
           <slot name="left"></slot>
@@ -13,9 +14,22 @@
         </div>
         </Col>
         <Col span="12"
-             style="padding: 16px;">
-        <div style="margin: -1em 0;">
-          <slot name="right"></slot>
+             style="padding: 16px; position: relative;">
+        <div style="position: absolute; text-align: center; bottom: 0; width: 100%;">
+          <Button type="text" @click="expandCollapseHandle">
+            <Icon type="ios-arrow-up" />
+            <Icon type="ios-arrow-down" />
+          </Button>
+        </div>
+        <div style="overflow: hidden; transition: height .2s ease-in-out;"
+             :style="{
+      height: cardHeight+'px'
+    }">
+          <div ref="codeRef">
+            <div style="margin: -2em 0;">
+              <slot name="right"></slot>
+            </div>
+          </div>
         </div>
         </Col>
       </Row>
@@ -35,14 +49,30 @@ export default {
   components: {},
   data () {
     return {
+      initLeftHeight: 0,
+      cardHeight: 0
     }
   },
   watch: {},
   computed: {},
-  methods: {},
+  methods: {
+    expandCollapseHandle () {
+      console.info('this.cardHeight', this.cardHeight)
+      console.info('this.$refs.codeRef.$el.offsetHeight', this.$refs.codeRef.offsetHeight)
+      if (this.cardHeight === this.initLeftHeight) {
+        this.cardHeight = this.$refs.codeRef.offsetHeight
+      } else {
+        this.cardHeight = this.initLeftHeight
+      }
+    },
+  },
   created () { },
   activated () { },
-  mounted () { },
+  mounted () {
+    console.info('this.$refs.leftRef.$el.offsetHeight ', this.$refs.leftRef.$el.offsetHeight)
+    this.initLeftHeight = this.$refs.leftRef.$el.offsetHeight + 16
+    this.cardHeight = this.initLeftHeight
+  },
   beforeDestroy () { }
 }
 </script>
